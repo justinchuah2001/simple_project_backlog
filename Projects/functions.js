aaad5d546b846edbc14b10a3675a78f4ab0d9ef1
Justin_Chuah <jchu0056@student.monash.edu>
2022-09-09 13:16:14 +0800

    Bug fixes

--- Projects/functions.js
+++ Projects/functions.js
@@ -5,8 +5,7 @@ function createCard()
     const saveModal = document.getElementById("saveTask")
     const closeModal = document.getElementById("cancelTask")
     
-    // Clear before opening prompt always
-    clearFields()
+    
     modal.showModal(); // Makes the prompt appear
     let cards = retrieveLSData()
     saveModal.addEventListener("click", ()=>{
@@ -31,27 +30,33 @@ function createCard()
             cards.push(card)
             updateLSData(cards)
             modal.close();
+            clearFields()
         } 
     })
     
     closeModal.addEventListener("click", () =>{
         modal.close();
+        clearFields()
     })
 }
+
+// Function to clear fields
 function clearFields(){
     //Getting references
     let taskName = document.getElementById("formName")
     let assginee = document.getElementById("assignedMembers")
     let tags = document.getElementById("tags")
     let status = document.getElementById("status")
+    let priority = document.getElementById("priority")
     let storyPoints = document.getElementById("storyPoints")
-    let details = document.getElementById("detailsp ")
+    let details = document.getElementById("details")
     
     //Resetting values
     taskName.value =""
-    assginee.value = ""
-    tags.value = ""
-    status.value = ""
+    assginee.value = "Justin"
+    tags.value = "Bugs"
+    priority.value = "High"
+    status.value = "To-Do"
     storyPoints.value = ""
     details.value = ""
 }
