8bfa6683aa2ecf4157e677dd682e9308ce66ee43
E Bing Sen <bingsene7744@gmail.com>
2022-10-16 02:09:40 +0800

    BUGFIX: Update codes

--- Projects/functions.js
+++ Projects/functions.js
@@ -70,6 +70,21 @@ function updateLSDataSprints(data) {
     localStorage.setItem("sprints", JSON.stringify(data))
 }
 
+function retrieveLSTeamMembers() {
+    if (localStorage.getItem("members") != null) {
+        return JSON.parse(localStorage.getItem("members"))
+    }
+    else {
+        intialization = []
+        localStorage.setItem("members", JSON.stringify(intialization))
+        return JSON.parse(localStorage.getItem("members"))
+    }
+}
+
+function updateLSTeamMembers(data) {
+    localStorage.setItem("members", JSON.stringify(data))
+}
+
 function checkValidity(card) {
     if (card.taskName == "" || card.storyPoints == "" || card.details == "") {
         alert("One or more fields are empty, please do not leave fields empty!")
@@ -81,6 +96,15 @@ function checkValidity(card) {
 }
 function checkValidity_Sprint(sprint) {
     if (sprint.sprintNumber == "" || sprint.sprintStatus == "" || sprint.startDate == "" || sprint.endDate == "") {
+        alert("One or more fields are empty, please do not leave fields empty!")
+        return false
+    }
+    else {
+        return true
+    }
+}
+function checkValidity_Members(member) {
+    if (member.memberName == "" || member.memberEmail == "" || member.memberRole == "" || member.memberTotalTimeContribution == "" || member.memberAvgTimeContributionPerDay == "") {
         alert("One or more fields are empty, please do not leave fields empty!")
         return false
     }
@@ -820,3 +844,121 @@ function showCardsInactive(){
         }
 }
 
+
+function addMember() {
+    const modal = document.getElementById("memberCreate")
+    const cancelModal = document.getElementById("cancelMember")
+
+    modal.showModal(); // Makes the prompt appear
+    //Closes the modal window once anything outside the window is clicked
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.close();
+        }
+
+        cancelModal.addEventListener("click", () => {
+            modal.close();
+            clearFieldsMember()
+        })
+    }
+}
+
+function showNewMember() {
+    for (let id_num = 1; id_num < retrieveLSTeamMembers().length + 1; id_num += 1) {
+        let members = retrieveLSTeamMembers()[id_num - 1]
+        if (members._inMemberList == true)
+        {
+            continue
+        }
+        // Adding new row for table (for each member)]
+        var table = document.getElementById("myTable"),
+        row = table.insertRow(-1),
+        cellButtons = row.insertCell(0),
+        cellName = row.insertCell(1),
+        cellEmail = row.insertCell(2),
+        cellRole = row.insertCell(3),
+        cellTTC = row.insertCell(4),
+        cellATCPD = row.insertCell(5);
+
+        // Buttons To Edit or Delete Members
+        cellButtons.innerHTML   =   `<td>
+                                        <button class= "edit_button" id="edit_member" onclick="editMember()">Edit</button>
+                                        <button class= "delete_button" id="delete_member" onclick="deleteMember()">Delete</button>
+                                    </td>`
+
+        // Add Details to table
+        cellName.innerHTML      =   `<td>
+                                        <span id="memberName${id_num}">${members._memberName}</span>
+                                    </td>`
+        cellEmail.innerHTML     =   `<td>
+                                        <span id="memberEmail${id_num}">${members._memberEmail}</span>
+                                    </td>`
+        cellRole.innerHTML      =   `<td>
+                                        <span id="memberRole${id_num}">${members._memberRole}</span>
+                                    </td>`
+        cellTTC.innerHTML       =   `<td>
+                                        <span id="memberTotalTime${id_num}">${members._memberTotalTimeContribution}</span>
+                                    </td>`
+        cellATCPD.innerHTML     =   `<td>
+                                        <span id="memberAvgTime${id_num}">${members._memberAvgTimeContributionPerDay}</span>
+                                    </td>`
+    }
+}
+
+// Function to clear fields for members
+function clearFieldsMember() {
+    //Getting references
+    let memberName = document.getElementById("memberName")
+    let memberEmail = document.getElementById("memberEmail")
+    let memberRole = document.getElementById("memberRole")
+    let memberTotalTime = document.getElementById("memberTotalTime")
+    let memberAvgTime = document.getElementById("memberAvgTime")
+
+    //Resetting values
+    memberName.value = ""
+    memberEmail.value = ""
+    memberRole.value = ""
+    memberTotalTime.value = ""
+    memberAvgTime.value = ""
+}
+
+  function saveMember() {
+    const modal = document.getElementById("membersCreate")
+    let members = retrieveLSTeamMembers();
+    // Initizalizing Members
+    let member = new Members();
+    // Retrieving input field values
+    member.memberName = document.getElementById("memberName").value
+    member.memberEmail = document.getElementById("memberEmail").value
+    member.memberRole = document.getElementById("memberRole").value
+    member.memberTotalTimeContribution = document.getElementById("memberTotalTime").value
+    member.memberAvgTimeContributionPerDay = document.getElementById("memberAvgTime").value
+
+    //Ensuring no empty fields
+    if (checkValidity_Members(member) == true) {
+        members.push(member)
+        updateLSTeamMembers(members)
+        showNewMember(member)
+        modal.close();
+        setTimeout(clearFields, 300)
+    }
+  }
+
+  function editMember()  {}
+
+  function deleteMember(id)  {
+    if (confirm("Are you sure you want to delete this member?") == true) {
+      let oldData = retrieveLSTeamMembers()
+      if (id - 1 == 0) {
+        oldData.splice(0, 1)
+      } 
+      else {
+        oldData.splice(id - 1, 1)
+      }
+
+      let newData = JSON.stringify(oldData)
+      localStorage.setItem("members", newData)
+      showNewMember() //Reupdate page with new ID
+      window.location.reload()
+    }
+  }
